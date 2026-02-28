cd1944d878623383d096fff7c7956be99a200182
Ranslmware <ranslmware40@gmail.com>
2026-02-28 00:46:30 -0300

    Classe Department criada

---
+++ src/main/java/application/Main.java
@@ -0,0 +1,12 @@
+package application;
+
+import model.entities.Department;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Department obj = new Department(1, "IT");
+        System.out.println(obj);
+
+    }
+}
---
+++ src/main/java/model/entities/Department.java
@@ -0,0 +1,57 @@
+package model.entities;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Department implements Serializable {
+
+    private Integer id;
+    private String name;
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    public Department(){
+
+    }
+
+    public Department(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (!(object instanceof Department that)) return false;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Department{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
