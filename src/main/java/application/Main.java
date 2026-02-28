d6972566dc35af41ca5185fc1252050899b4681d
Ranslmware <ranslmware40@gmail.com>
2026-02-28 03:12:13 -0300

    Criando métodos auxiliares

--- src/main/java/application/Main.java
+++ src/main/java/application/Main.java
@@ -10,7 +10,7 @@ public class Main {
     public static void main(String[] args) {
 
         SellerDao sellerDao = DaoFactory.createSellerDao();
-
+        System.out.println("--- Test 1: seller findById ---");
         Seller seller = sellerDao.findById(3);
 
         System.out.println(seller);
